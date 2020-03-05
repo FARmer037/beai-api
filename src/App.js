712053bfb39cbff6f50161b50ce8d93fa2b0e403
FARmer037 <43213155+FARmer037@users.noreply.github.com>
2020-03-05 11:50:47 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,8 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React from 'react'
 import BearList from './components/BearList'
 import InputForm from './components/InputForm';
-import { useSelector, useDispatch } from 'react-redux'
 
 export default () => {
-
-  const dispatch = useDispatch();
-
-  const [formData, setData] = useState({
-    name: '',
-    weight: 0,
-    img: ''
-  });
-
-  const { name, weight, img } = formData;
-
-  const getBears = async () => {
-    const result = await axios.get(`http://localhost/api/bears`)
-    // setBears(result.data)
-
-    const action = {type: 'GET_BEAR', bears: result.data}
-
-    dispatch(action)
-  }
-
-  useEffect(() => {
-    getBears()
-  }, [])
 
   return (
     <div>
